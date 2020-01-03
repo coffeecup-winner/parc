a0dfb6b650afaffc0143e58cece5bee44b6acd05
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2020-01-02 21:11:57 -0800

    Add curses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use pancurses::{initscr, endwin, Input, noecho};
+
 fn main() {
-    println!("Hello, world!");
+    let window = initscr();
+    window.printw("Hello, curses!");
+    window.refresh();
+    window.keypad(true);
+    noecho();
+    loop {
+        match window.getch() {
+            Some(Input::KeyF10) => break,
+            Some(input) => {
+                window.erase();
+                window.printw(format!("{:?}\n", input));
+            }
+            None => ()
+        }
+    }
+    endwin();
 }
