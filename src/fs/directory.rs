ff557fd1aef5d5a86fdaab7615478cf5881ac671
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2020-01-04 19:09:24 -0800

    Move FS-related data types into a separate module

---
+++ src/fs/directory.rs
@@ -0,0 +1,69 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
+pub struct FsEntry {
+    pub name: String,
+    pub type_: fs::FileType,
+    pub metadata: fs::Metadata,
+}
+
+pub struct Directory {
+    path: PathBuf,
+    entries: Vec<FsEntry>,
+}
+
+impl Directory {
+    pub fn open(path: &Path) -> Directory {
+        let mut directory = Directory {
+            path: path.to_path_buf(),
+            entries: vec!(),
+        };
+        directory.refresh();
+        directory
+    }
+
+    fn refresh(&mut self) {
+        self.entries.clear();
+        if let Ok(d) = fs::read_dir(&self.path) {
+            for x in d {
+                if let Ok(de) = x {
+                    self.entries.push(FsEntry {
+                        name: de.file_name().into_string().unwrap(),
+                        type_: de.file_type().unwrap(),
+                        metadata: de.metadata().unwrap(),
+                    });
+                }
+            }
+        }
+    }
+
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+
+    pub fn entries(&self) -> &Vec<FsEntry> {
+        &self.entries
+    }
+
+    pub fn open_subdirectory(&mut self, idx: usize) -> bool {
+        if idx > self.entries.len() {
+            return false;
+        }
+        let entry = &self.entries[idx];
+        if entry.type_.is_dir() {
+            self.path.push(&entry.name);
+            self.refresh();
+            return true;
+        }
+        return false;
+    }
+
+    pub fn open_parent(&mut self) -> bool {
+        if let Some(parent) = self.path.parent() {
+            self.path = parent.to_path_buf();
+            self.refresh();
+            return true;
+        }
+        return false;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
 use std::cmp::{max, min};
-use std::fs;
 use std::path::Path;
 
 use pancurses::{cbreak, curs_set, endwin, initscr, noecho, Input, A_BOLD};
 
-struct FsEntry {
-    pub name: String,
-    pub type_: fs::FileType,
-    pub metadata: fs::Metadata,
-}
+use crate::fs::directory::Directory;
 
-fn list_directory(path: &Path) -> Vec<FsEntry> {
-    let mut result = vec![];
-    match fs::read_dir(path) {
-        Err(_) => {}
-        Ok(d) => {
-            for x in d {
-                match x {
-                    Err(_) => {}
-                    Ok(de) => {
-                        result.push(FsEntry {
-                            name: de.file_name().into_string().unwrap(),
-                            type_: de.file_type().unwrap(),
-                            metadata: de.metadata().unwrap(),
-                        });
-                    }
-                }
-            }
-        }
-    }
-    result
-}
+mod fs;
 
 fn main() {
     let window = initscr();
@@ -39,15 +14,14 @@ fn main() {
     cbreak();
     curs_set(0);
     noecho();
+    let mut directory = Directory::open(&Path::new(".").canonicalize().unwrap());
     let mut selected_line_idx = 0;
-    let mut current_path = Path::new(".").canonicalize().unwrap();
     let mut status_line = String::new();
     loop {
         window.erase();
-        let contents = list_directory(&current_path);
-        window.mvprintw(0, 0, current_path.to_str().unwrap());
+        window.mvprintw(0, 0, directory.path().to_str().unwrap());
         let mut y = 1;
-        for filename in &contents {
+        for filename in directory.entries() {
             window.mvprintw(y, 0, &filename.name);
             y += 1;
         }
@@ -57,20 +31,20 @@ fn main() {
         match window.getch() {
             Some(Input::KeyF10) | Some(Input::Character('q')) => break,
             Some(Input::KeyDown) => {
-                selected_line_idx = min(contents.len() as i32 - 1, selected_line_idx + 1);
+                selected_line_idx = min(directory.entries().len() as i32 - 1, selected_line_idx + 1);
             }
             Some(Input::KeyUp) => {
                 selected_line_idx = max(0, selected_line_idx - 1);
             }
             Some(Input::KeyEnter) | Some(Input::Character('\n')) => {
-                if contents[selected_line_idx as usize].type_.is_dir() {
-                    current_path.push(&contents[selected_line_idx as usize].name);
+                if directory.open_subdirectory(selected_line_idx as usize) {
                     selected_line_idx = 0;
                 }
             }
             Some(Input::KeyBackspace) | Some(Input::Character('\u{7f}')) => {
-                current_path = current_path.parent().unwrap_or(&current_path).to_path_buf();
-                selected_line_idx = 0;
+                if directory.open_parent() {
+                    selected_line_idx = 0;
+                }
             }
             Some(input) => {
                 status_line = format!("Unhandled input: {:?}", input);
